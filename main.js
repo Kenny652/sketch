22d7dcbc0e7640e6721a2241b947f30ffda52460
kenny652 <brignol.com@gmail.com>
2025-03-09 17:00:57 -0400

    Finalized the project

--- main.js
+++ main.js
@@ -1,7 +1,22 @@
 let color = 'black';
+let click = false;
 
 document.addEventListener('DOMContentLoaded', function() {
     createBoard(16)
+
+    document.querySelector('body').addEventListener('click', function(e){
+        if(e.target.tagName != 'BUTTON') {
+            click = !click;
+            let draw = document.querySelector('#draw');
+            if (click){
+                draw.innerHTML = 'Now you can Draw';
+            }
+            else{
+                draw.innerHTML = `You're now allowed to draw`
+            }
+        }
+    })
+
     let btn = document.querySelector('#popup');
     btn.addEventListener('click', function(){
         let size = Number(getSize());
@@ -39,11 +54,13 @@ function getSize() {
 }
 
 function colorDiv() {
-    if (color == 'random'){
-        this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`
-    }
-    else {
-        this.style.backgroundColor = 'black'
+    if(click) {
+        if (color == 'random'){
+            this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`
+        }
+        else {
+            this.style.backgroundColor = 'black'
+        }
     }
 }
 
