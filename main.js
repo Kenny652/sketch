63b0f662c0fd261362afd27bcb96104b038c3a16
kenny652 <brignol.com@gmail.com>
2025-03-09 16:12:45 -0400

    Enable the buttons

--- main.js
+++ main.js
@@ -1,5 +1,12 @@
+let color = 'black';
+
 document.addEventListener('DOMContentLoaded', function() {
-    createBoard(40)
+    createBoard(16)
+    let btn = document.querySelector('#popup');
+    btn.addEventListener('click', function(){
+        let size = Number(getSize());
+        createBoard(size);
+    })
 })
 
 function createBoard(size) {
@@ -11,8 +18,40 @@ function createBoard(size) {
 
     for (let i = 0; i < numDivs; i++) {
         let div = document.createElement('div');
-        div.style.backgroundColor = 'yellow'
+        div.addEventListener('mouseover', colorDiv)
         board.insertAdjacentElement('beforeend', div);        
     }
 }
 
+function getSize() {
+    let input = prompt('what will be the size of the board');
+    let message = document.querySelector('#message');
+    if( input == ''){
+        message.innerHTML = 'Please provide a number';
+    }
+    else if (input < 0 || input > 100) {
+        message.innerHTML = 'Provide a number between 1 and 100';
+    }
+    else {
+        message.innerHTML = 'You can play now';
+        return input;
+    } 
+}
+
+function colorDiv() {
+    if (color == 'random'){
+        this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`
+    }
+    else {
+        this.style.backgroundColor = 'black'
+    }
+}
+
+function getColor(colorChoice) {
+    color = colorChoice;
+}
+
+function resetBoard() {
+    let divs = document.querySelectorAll('div');
+    divs.forEach((div) => div.style.backgroundColor = 'white')
+}
